718558467cdade32fbcac5c1ae06425a0bcee59d
GlobalDatafeeds <billing@globaldatafeeds.in>
2026-03-09 11:47:18 +0530

    Update webhook URLs in master_webhook.php

--- master_webhook.php
+++ master_webhook.php
@@ -6,6 +6,8 @@
 $webhook1_url = "https://ccavenue-render-integrations-1.onrender.com/ccavenue_webhook.php"; // Payment Status
 //$webhook2_url = "https://ccavenue-render-integrations.onrender.com/webhook.php"; 
 
+
+//https://ccavenue-render-integrations-1.onrender.com/
 $webhook2_url = "https://ccavenue-render-integrations-1.onrender.com/webhook.php";        // Deal / Automation
 
 // ---------------- LOG RECEIVED DATA ----------------
